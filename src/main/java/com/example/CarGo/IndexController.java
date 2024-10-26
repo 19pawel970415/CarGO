d57ffc5c129caba44bac71c2f9a76c5dea4d907e
19Pawel97 <19pawel9772@gmail.com>
2024-10-26 22:03:33 +0200

    frontend init

--- src/main/java/com/example/CarGo/IndexController.java
+++ src/main/java/com/example/CarGo/IndexController.java
@@ -1,13 +1,38 @@
 package com.example.CarGo;
 
+import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RestController;
 
-@RestController
+@Controller
 public class IndexController {
 
     @GetMapping("/")
-    public String sayHello() {
-        return "Welcome to CarGo :)";
+    public String showIndex() {
+        return "index";
+    }
+
+    @GetMapping("/about")
+    public String showAbout() {
+        return "about";
+    }
+
+    @GetMapping("/client")
+    public String showClient() {
+        return "client";
+    }
+
+    @GetMapping("/contact")
+    public String showContact() {
+        return "contact";
+    }
+
+    @GetMapping("/gallery")
+    public String showGallery() {
+        return "gallery";
+    }
+
+    @GetMapping("/services")
+    public String showServices() {
+        return "services";
     }
 }
