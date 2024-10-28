2f37eb8dd7d01c70c2c64e239a8395a1d5ecbf25
DiegoLut <kuba200110@gmail.com>
2024-10-28 10:29:56 +0100

    html css js separation from login and register

---
+++ src/main/resources/static/js/script.js
@@ -0,0 +1,9 @@
+function validatePasswords() {
+    const password = document.getElementById('password').value;
+    const confirmPassword = document.getElementById('confirmPassword').value;
+    if (password !== confirmPassword) {
+        alert('Passwords do not match. Please try again.');
+        return false;
+    }
+    return true;
+}
