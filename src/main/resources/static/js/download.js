dd72374067282378b679fb64005fd38eedea0f45
19Pawel97 <19pawel9772@gmail.com>
2024-11-13 16:31:59 +0100

    We are CarGo! file download confirmation added

--- src/main/resources/static/js/download.js
+++ src/main/resources/static/js/download.js
@@ -1,3 +1,7 @@
 function downloadFile() {
-    window.location.href = '/downloadFile';
+    const userConfirmed = confirm('Download "We are CarGo!" file to read more about us. Do you agree to download the file?');
+
+    if (userConfirmed) {
+        window.location.href = '/downloadFile';
+    }
 }
