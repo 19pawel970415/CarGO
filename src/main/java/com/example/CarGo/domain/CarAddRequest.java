79894c656298b37f015ffe09fb93120d069a5638
DiegoLut <kuba200110@gmail.com>
2025-01-15 10:10:55 +0100

    stats page -> registration number and vin number added to statistics page for easier identification of car

--- src/main/java/com/example/CarGo/domain/CarAddRequest.java
+++ src/main/java/com/example/CarGo/domain/CarAddRequest.java
@@ -17,5 +17,93 @@ public class CarAddRequest {
     private SeatCount seatCount;
     private Double pricePerDay;
     private Location location;
+
+    public CarMake getMake() {
+        return make;
+    }
+
+    public void setMake(CarMake make) {
+        this.make = make;
+    }
+
+    public String getModel() {
+        return model;
+    }
+
+    public void setModel(String model) {
+        this.model = model;
+    }
+
+    public String getRegistrationNumber() {
+        return registrationNumber;
+    }
+
+    public void setRegistrationNumber(String registrationNumber) {
+        this.registrationNumber = registrationNumber;
+    }
+
+    public String getVin() {
+        return vin;
+    }
+
+    public void setVin(String vin) {
+        this.vin = vin;
+    }
+
+    public int getYearOfProduction() {
+        return yearOfProduction;
+    }
+
+    public void setYearOfProduction(int yearOfProduction) {
+        this.yearOfProduction = yearOfProduction;
+    }
+
+    public ChassisType getChassisType() {
+        return chassisType;
+    }
+
+    public void setChassisType(ChassisType chassisType) {
+        this.chassisType = chassisType;
+    }
+
+    public GearboxType getGearboxType() {
+        return gearboxType;
+    }
+
+    public void setGearboxType(GearboxType gearboxType) {
+        this.gearboxType = gearboxType;
+    }
+
+    public FuelType getFuelType() {
+        return fuelType;
+    }
+
+    public void setFuelType(FuelType fuelType) {
+        this.fuelType = fuelType;
+    }
+
+    public SeatCount getSeatCount() {
+        return seatCount;
+    }
+
+    public void setSeatCount(SeatCount seatCount) {
+        this.seatCount = seatCount;
+    }
+
+    public Double getPricePerDay() {
+        return pricePerDay;
+    }
+
+    public void setPricePerDay(Double pricePerDay) {
+        this.pricePerDay = pricePerDay;
+    }
+
+    public Location getLocation() {
+        return location;
+    }
+
+    public void setLocation(Location location) {
+        this.location = location;
+    }
 }
 
